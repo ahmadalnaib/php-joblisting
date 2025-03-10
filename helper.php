d9aae4c799f3857806ded3476a42c16c660f18b4
ahmedhabeeb73 <alnaib888@gmail.com>
2025-03-10 12:27:35 +0100

    add helper function

--- helper.php
+++ helper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  Get the base path of the project
  * 
@@ -6,9 +7,10 @@
  * @return string
  */
 
-  function base_path($path = '') {
-    return __DIR__ . '/' . $path;
- }
+function base_path($path = '')
+{
+  return __DIR__ . '/' . $path;
+}
 
 /**
  * Load view
@@ -17,31 +19,58 @@
  * @return void
  */
 
- function load_view($name)
-  {
-    $viewBath= base_path("views/{$name}.view.php");
-    if(file_exists($viewBath)) {
-        require $viewBath;
-    } else {
-        echo "View '{$name} not found' ";
-    }
- }
+function load_view($name)
+{
+  $viewBath = base_path("views/{$name}.view.php");
+  if (file_exists($viewBath)) {
+    require $viewBath;
+  } else {
+    echo "View '{$name} not found' ";
+  }
+}
 
 
- /**
+/**
  * Load partials
  * 
  * @param string $name
  * @return void
  */
 
-  function load_partial($name) {
-      $partialBath= base_path("views/partials/{$name}.php");
-      if(file_exists($partialBath)) {
-          require $partialBath;
-      } else {
-          echo "Partial '{$name} not found' ";
-      }
+function load_partial($name)
+{
+  $partialBath = base_path("views/partials/{$name}.php");
+  if (file_exists($partialBath)) {
+    require $partialBath;
+  } else {
+    echo "Partial '{$name} not found' ";
   }
+}
 
 
+
+/**
+ * Inspact the value
+ * @param mixed $value
+ * @return void
+ */
+function dd($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+}
+
+/**
+ * Inspact the value and die
+ * @param mixed $value
+ * @return void
+ */
+
+function ddd($value)
+{
+  echo "<pre>";
+  var_dump($value);
+  echo "</pre>";
+  die();
+}
