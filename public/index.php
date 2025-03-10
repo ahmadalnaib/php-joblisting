9d826c49e0f0401d200820b81b5e32e74ec7c17d
ahmedhabeeb73 <alnaib888@gmail.com>
2025-03-10 15:43:31 +0100

    create routeing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 require "../helper.php";
-load_view("home");
+// load_view("home");
+
+$routers = [
+  '/' => "controllers/home.php",
+  '/listings' => "controllers/listings/index.php",
+  '/listings/show' => "controllers/listings/show.php",
+  '/listings/create' => "controllers/listings/create.php",
+  '/listings/store' => "controllers/listings/store.php",
+  '/listings/edit' => "controllers/listings/edit.php",
+  '/listings/update' => "controllers/listings/update.php",
+  '/listings/delete' => "controllers/listings/delete.php",
+  '/listings/search' => "controllers/listings/search.php",
+  '404' => "controllers/error/404.php"
+
+];
+
+$uri= $_SERVER['REQUEST_URI'];
+if(array_key_exists($uri, $routers)){
+  require base_path($routers[$uri]);
+}else{
+  require base_path($routers['404']);
+}
